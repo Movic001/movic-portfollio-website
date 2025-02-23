7cd6d7536f746bdfcc17f6a386c3fa4081db2bfc
Etzonyx <ibrahimabdulkerim001@gmail.com>
2025-02-22 18:13:40 -0800

    initial changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,10 +10,10 @@ class NavBar extends Component {
             <div className="container-fluid">
               <a className="navbar-brand text-success" href="service">Portfollio</a>
 
-              <button className="navbar-toggler" type="button"data-bs-toggle="collapse" data-bs-target="servicenavbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <button className="navbar-toggler" type="button"data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon text-light"> go</span>
               </button>
-
+            
               <div className="collapse navbar-collapse " style={{justifyContent:'center'}} id="navbarSupportedContent" >
                 <ul className="navbar-nav d-flex">
 
