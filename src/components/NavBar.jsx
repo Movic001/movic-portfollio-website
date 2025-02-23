c3cfd5b9654aa53380a66cd6bee4e51fd5ba913c
Etzonyx <ibrahimabdulkerim001@gmail.com>
2025-02-22 18:18:25 -0800

    initial changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ class NavBar extends Component {
               <a className="navbar-brand text-success" href="service">Portfollio</a>
 
               <button className="navbar-toggler" type="button"data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon text-light"> go</span>
+                <span className="navbar-toggler-icon text-light"> <i class="fa-solid fa-bars"></i></span>
               </button>
             
               <div className="collapse navbar-collapse " style={{justifyContent:'center'}} id="navbarSupportedContent" >
